905d46ab1772b5b63f34d840460af34c041e02ad
Lynn Garren <garren@fnal.gov>
2014-10-01 14:57:50 -0500

    need mrb g -r

--- buildScripts/buildUboone.sh
+++ buildScripts/buildUboone.sh
@@ -13,9 +13,10 @@ echo "workspace: $WORKSPACE"
 
 ncores=`cat /proc/cpuinfo 2>/dev/null | grep -c -e '^processor'`
 
-set -x
 
 source /grid/fermiapp/products/larsoft/setup || exit 1
+
+set -x
 setup git || exit 1
 setup gitflow || exit 1
 setup mrb || exit 1
@@ -25,10 +26,13 @@ mkdir -p $WORKSPACE/temp || exit 1
 mkdir -p $WORKSPACE/copyBack || exit 1
 cd $WORKSPACE/temp || exit 1
 mrb newDev  -v $UBOONE -q $QUAL:$BUILDTYPE || exit 1
+
 source localProducts*/setup || exit 1
+
 cd $MRB_SOURCE  || exit 1
-mrb g -t $UBOONE uboonecode || exit 1
-mrb g -t $UBUTIL ubutil || exit 1
+# make sure we get a read-only copy
+mrb g -r -t $UBOONE uboonecode || exit 1
+mrb g -r -t $UBUTIL ubutil || exit 1
 cd $MRB_BUILDDIR || exit 1
 mrb b -j$ncores || exit 1
 mrb mp -j$ncores || exit 1
