6c7e31a1b5a2b4e6ab0d90af7d89622907bd7d00
Lynn Garren <garren@fnal.gov>
2015-03-10 17:39:37 -0500

    use mrb v1_04_01 and build distribution manifest

--- buildScripts/buildUboone.sh
+++ buildScripts/buildUboone.sh
@@ -33,6 +33,18 @@ set +x
 
 source localProducts*/setup || exit 1
 
+# some shenanigans so we can use mrb v1_04_01
+cd $MRB_INSTALL
+curl --fail --silent --location --insecure -O http://scisoft.fnal.gov/scisoft/packages/mrb/v1_04_01/mrb-1.04.01-noarch.tar.bz2  || \
+      { cat 1>&2 <<EOF
+ERROR: pull of http://scisoft.fnal.gov/scisoft/packages/mrb/v1_04_01/mrb-1.04.01-noarch.tar.bz2 failed
+EOF
+        exit 1
+      }
+tar xf mrb-1.04.01-noarch.tar.bz2 || exit 1
+setup mrb  || exit 1
+which mrb
+
 set -x
 cd $MRB_SOURCE  || exit 1
 # make sure we get a read-only copy
@@ -41,8 +53,13 @@ mrb g -r -t $UBUTIL ubutil || exit 1
 cd $MRB_BUILDDIR || exit 1
 mrbsetenv || exit 1
 mrb b -j$ncores || exit 1
-mrb mp -j$ncores || exit 1
+mrb mp -n uboone -- -j$ncores || exit 1
+# add uboone_data to the manifest
+uboone_data_version=`grep uboone_data $MRB_SOURCE/uboonecode/ups/product_deps  | grep -v qualifier | sed -e 's/[ \t]\{1,\}/ /g' | cut -f2 -d" "`
+uboone_data_dot_version=`echo ${uboone_data_version} |  sed -e 's/_/./g' | sed -e 's/^v//'`
+echo "uboone_data        ${uboone_data_version}       uboone_data-${uboone_data_dot_version}-noarch.tar.gz" >>  uboone-*_MANIFEST.txt
 mv *.bz2  $WORKSPACE/copyBack/ || exit 1
+mv uboone*.txt  $WORKSPACE/copyBack/ || exit 1
 ls -l $WORKSPACE/copyBack/
 cd $WORKSPACE || exit 1
 rm -rf $WORKSPACE/temp || exit 1
