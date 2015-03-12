2333a3fb3a3eaa9804a3d9e2fcdbb356f23cd6e5
Herbert Greenlee <greenlee@fnal.gov>
2015-03-12 15:46:35 -0500

    Only fetch getopt v1_1_6 for macos.

--- buildScripts/buildUboone.sh
+++ buildScripts/buildUboone.sh
@@ -52,16 +52,18 @@ set +x
 source localProducts*/setup || exit 1
 
 # some shenanigans so we can use getopt v1_1_6
-cd $MRB_INSTALL
-curl --fail --silent --location --insecure -O http://scisoft.fnal.gov/scisoft/packages/getopt/v1_1_6/getopt-1.1.6-d13-x86_64.tar.bz2 || \
+if [ `uname` = Darwin ]; then
+  cd $MRB_INSTALL
+  curl --fail --silent --location --insecure -O http://scisoft.fnal.gov/scisoft/packages/getopt/v1_1_6/getopt-1.1.6-d13-x86_64.tar.bz2 || \
       { cat 1>&2 <<EOF
 ERROR: pull of http://scisoft.fnal.gov/scisoft/packages/getopt/v1_1_6/getopt-1.1.6-d13-x86_64.tar.bz2 failed
 EOF
         exit 1
       }
-tar xf getopt-1.1.6-d13-x86_64.tar.bz2 || exit 1
-setup getopt v1_1_6  || exit 1
-which getopt
+  tar xf getopt-1.1.6-d13-x86_64.tar.bz2 || exit 1
+  setup getopt v1_1_6  || exit 1
+  which getopt
+fi
 
 set -x
 cd $MRB_SOURCE  || exit 1
