2a535c5264bc303cefce91584e39c37b27250145
Tingjun Yang <tjyang@fnal.gov>
2015-08-13 23:34:57 -0500

    add scripts to build dunetpc and duneutil

---
+++ buildScripts/buildDUNE.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# build lbnecode 
+# use mrb
+# designed to work on Jenkins
+# this is a proof of concept script
+
+echo "dunetpc version: $DUNEVER"
+echo "duneutil version: $DUNEUTIL"
+echo "base qualifiers: $QUAL"
+echo "build type: $BUILDTYPE"
+echo "workspace: $WORKSPACE"
+
+ncores=`cat /proc/cpuinfo 2>/dev/null | grep -c -e '^processor'`
+
+source /grid/fermiapp/lbne/software/setup_lbne.sh || exit 1
+
+setup git || exit 1
+setup gitflow || exit 1
+setup mrb || exit 1
+export MRB_PROJECT=dune
+which mrb
+
+set -x
+rm -rf $WORKSPACE/temp || exit 1
+mkdir -p $WORKSPACE/temp || exit 1
+mkdir -p $WORKSPACE/copyBack || exit 1
+rm -f $WORKSPACE/copyBack/* || exit 1
+cd $WORKSPACE/temp || exit 1
+mrb newDev  -v $DUNEVER -q $QUAL:$BUILDTYPE || exit 1
+set +x
+
+source localProducts*/setup || exit 1
+
+set -x
+cd $MRB_SOURCE  || exit 1
+# make sure we get a read-only copy
+mrb g -r -t $DUNEVER dunetpc || exit 1
+mrb g -r -t $DUNEUTIL duneutil || exit 1
+cd $MRB_BUILDDIR || exit 1
+mrbsetenv || exit 1
+mrb b -j$ncores || exit 1
+mrb mp -j$ncores || exit 1
+mv *.bz2  $WORKSPACE/copyBack/ || exit 1
+ls -l $WORKSPACE/copyBack/
+cd $WORKSPACE || exit 1
+rm -rf $WORKSPACE/temp || exit 1
+set +x
+
+exit 0
