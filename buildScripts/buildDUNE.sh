145f7b0341b47717ae44e7119403a45978580399
Tingjun Yang <tjyang@fnal.gov>
2015-09-24 10:39:20 -0500

    Make buildDUNE.sh work with mac.

--- buildScripts/buildDUNE.sh
+++ buildScripts/buildDUNE.sh
@@ -11,14 +11,28 @@ echo "base qualifiers: $QUAL"
 echo "build type: $BUILDTYPE"
 echo "workspace: $WORKSPACE"
 
-ncores=`cat /proc/cpuinfo 2>/dev/null | grep -c -e '^processor'`
+if [ `uname` = Darwin ]; then
+  #ncores=`sysctl -n hw.ncpu`
+  #ncores=$(( $ncores / 4 ))
+  ncores=1
+else
+  ncores=`cat /proc/cpuinfo 2>/dev/null | grep -c -e '^processor'`
+fi
+if [ $ncores -lt 1 ]; then
+  ncores=1
+fi
+echo "Building using $ncores cores."
 
 source /grid/fermiapp/products/dune/setup_dune.sh || exit 1
 
-setup git || exit 1
+# Use system git on macos.
+
+if ! uname | grep -q Darwin; then
+  setup git || exit 1
+fi
 setup gitflow || exit 1
-setup mrb || exit 1
 export MRB_PROJECT=dune
+echo "Mrb path:"
 which mrb
 
 set -x
@@ -31,6 +45,20 @@ mrb newDev  -v $DUNEVER -q $QUAL:$BUILDTYPE || exit 1
 set +x
 
 source localProducts*/setup || exit 1
+
+# some shenanigans so we can use getopt v1_1_6
+if [ `uname` = Darwin ]; then
+#  cd $MRB_INSTALL
+#  curl --fail --silent --location --insecure -O http://scisoft.fnal.gov/scisoft/packages/getopt/v1_1_6/getopt-1.1.6-d13-x86_64.tar.bz2 || \
+#      { cat 1>&2 <<EOF
+#ERROR: pull of http://scisoft.fnal.gov/scisoft/packages/getopt/v1_1_6/getopt-1.1.6-d13-x86_64.tar.bz2 failed
+#EOF
+#        exit 1
+#      }
+#  tar xf getopt-1.1.6-d13-x86_64.tar.bz2 || exit 1
+  setup getopt v1_1_6  || exit 1
+#  which getopt
+fi
 
 set -x
 cd $MRB_SOURCE  || exit 1
