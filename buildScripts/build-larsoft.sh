59c59bd4c5d9354aa19e77d000d47322606fdee3
Lynn Garren <garren@fnal.gov>
2018-11-05 13:03:45 -0600

    add s69

--- buildScripts/build-larsoft.sh
+++ buildScripts/build-larsoft.sh
@@ -222,6 +222,18 @@ case ${qual_set} in
      d16_ok=true
      d14_ok=false
   ;;
+  s69:e17)
+     basequal=e17
+     squal=s69
+     d16_ok=true
+     d14_ok=false
+  ;;
+  s69:c2)
+     basequal=c2
+     squal=s69
+     d16_ok=true
+     d14_ok=false
+  ;;
   s70:e17)
      basequal=e17
      squal=s70
