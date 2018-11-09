8b25398ad5d5be24d7931c235f9b1aa8584e49c6
Thomas Junk <trj@fnal.gov>
2018-11-09 14:38:35 -0600

    fix bug in mrb version fallback on macOS

--- buildScripts/buildDUNE.sh
+++ buildScripts/buildDUNE.sh
@@ -57,15 +57,21 @@ else
   exit 1
 fi
 
-# Use system git on macos, and skip around a version of mrb that does not work on macOS
+# Use git out of ups except use the system git on macos
 
 if ! uname | grep -q Darwin; then
   setup git || exit 1
+fi
+
+# skip around a version of mrb that does not work on macOS
+
+if [ `uname` = Darwin ]; then
   if [[ x`which mrb | grep v1_17_02` != x ]]; then
     unsetup mrb || exit 1
     setup mrb v1_16_02 || exit 1
   fi
 fi
+
 setup gitflow || exit 1
 export MRB_PROJECT=dune
 echo "Mrb path:"
