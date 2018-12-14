55806d51bb1f18092f8ec9941a20f88b14de7daa
Thomas Junk <trj@fnal.gov>
2018-12-14 12:37:47 -0600

    only use the /grid/fermiapp setup script on macOS

--- buildScripts/buildDUNEutil.sh
+++ buildScripts/buildDUNEutil.sh
@@ -33,9 +33,12 @@ if [ $ncores -lt 1 ]; then
 fi
 echo "Building using $ncores cores."
 
-if [ -f /grid/fermiapp/products/dune/setup_dune_fermiapp.sh ]; then
+if [ `uname` = Darwin -a -f /grid/fermiapp/products/dune/setup_dune_fermiapp.sh ]; then
   source /grid/fermiapp/products/dune/setup_dune_fermiapp.sh || exit 1
 elif [ -f /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh ]; then
+  if [ -x /cvmfs/grid.cern.ch/util/cvmfs-uptodate ]; then
+    /cvmfs/grid.cern.ch/util/cvmfs-uptodate /cvmfs/dune.opensciencegrid.org/products
+  fi
   source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh || exit 1
 else
   echo "No setup file found."
