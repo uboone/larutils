c773a8809e4085ffa864e7412ccce4e56f8fbc1c
Lynn Garren <garren@fnal.gov>
2019-02-04 15:30:31 -0600

    add s75 for art v3_01_01

--- buildScripts/build-larsoft.sh
+++ buildScripts/build-larsoft.sh
@@ -264,6 +264,18 @@ case ${qual_set} in
      d16_ok=true
      d14_ok=false
   ;;
+  s75:e17)
+     basequal=e17
+     squal=s75
+     d16_ok=true
+     d14_ok=false
+  ;;
+  s75:c2)
+     basequal=c2
+     squal=s75
+     d16_ok=true
+     d14_ok=false
+  ;;
   *)
     usage
     exit 1
