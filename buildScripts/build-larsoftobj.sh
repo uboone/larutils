b2a46ea3f69f3fd69b653183c55f32c83e1fe913
Lynn Garren <garren@fnal.gov>
2019-08-14 11:03:23 -0500

    modernize

--- buildScripts/build-larsoftobj.sh
+++ buildScripts/build-larsoftobj.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-# pull source code in $WORKSPACE/source
-# build in $WORKSPACE/build
-# copyback directory is $WORKSPACE/copyBack
+# pull source code in ${working_dir}/source
+# build in ${working_dir}/build
+# copyback directory is ${working_dir}/copyBack
 
 usage()
 {
   cat 1>&2 <<EOF
 Usage: $(basename ${0}) [-h]
+       env WORKSPACE=<workspace> LVER=<larsoftobj version> QUAL=<qualifier> BUILDTYPE=<debug|prof> $(basename ${0}) 
        $(basename ${0}) <version> <qual_set> <build_type>
 
 Options:
 
   -h    This help.
 
-Arguments:
-
-  qual_set         Supported qualifier sets: s8:e7, s11:e7
-
 EOF
 }
+
+have_label() {
+  for label in "${labels[@]}"; do
+    for wanted in "$@"; do
+      [[ "${label}" == "${wanted}" ]] && return 0
+    done
+  done
+  return 1
+}
+
 
 while getopts :h OPT; do
   case ${OPT} in
@@ -35,76 +42,39 @@ done
 shift `expr $OPTIND - 1`
 OPTIND=1
 
-working_dir=${WORKSPACE}
-version=${LVER}
-qual_set="${QUAL}"
-build_type=${BUILDTYPE}
+working_dir="${WORKSPACE:-$(pwd)}"
+version="${1:-${LVER}}"
+qual_set="${2:-${QUAL}}"
+oIFS=${IFS}; IFS=:; quals=(${qual_set//-/:}); IFS=$oIFS; unset oIFS
+build_type="${3:-${BUILDTYPE}}"
 
-d16_ok=true
-basequal=${qual_set}
-
-case ${qual_set} in
-  e9) d16_ok=false ;;
-  e10) d16_ok=false ;;
-  e1[457]) ;;
-  c2) d14_ok=false ;;
-  *)
-    usage
-    exit 1
-esac
+labels=()
+for onequal in "${quals[@]}"; do
+  case ${onequal} in
+    e[679]|e1[0-9]|c[0-9])
+      basequal=${onequal}
+      ;;
+    s7[0-9]|s8[0-9])
+      squal=${onequal}
+      ;;
+    *)
+      labels+=${onequal}
+  esac
+done
 
 case ${build_type} in
-  debug) ;;
-  prof) ;;
+  debug) qflag="-d" ;;
+  prof) qflag="-p" ;;
   *)
     usage
     exit 1
 esac
 
 # create copyBack so artifact copy does not fail on early exit
-rm -rf $WORKSPACE/copyBack 
-mkdir -p $WORKSPACE/copyBack || exit 1
+rm -rf "${working_dir}/copyBack"
+mkdir -p "${working_dir}/copyBack" || exit 1
 
-# check XCode
-if [[ `uname -s` == Darwin ]] 
-then
-  OSnum=`uname -r | cut -f1 -d"."`
-  xver=`xcodebuild -version | grep Xcode | cut -f2 -d" " | cut -f1 -d"."`
-  xcver=`xcodebuild -version | grep Xcode`
-  if [[ ${basequal} == e9 ]] && [[ ${xver} < 7 ]] && [[ ${OSnum} > 13 ]]
-  then
-    echo "${basequal} build not supported on `uname -s`${OSnum} with ${xcver}"
-    echo "${basequal} build not supported on `uname -s`${OSnum} with ${xcver}" > $WORKSPACE/copyBack/skipping_build
-    exit 0
-  elif [[ ${basequal} == e1[04] ]] && [[ ${xver} < 7 ]] && [[ ${OSnum} > 13 ]]
-  then
-    echo "${basequal} build not supported on `uname -s`${OSnum} with ${xcver}"
-    echo "${basequal} build not supported on `uname -s`${OSnum} with ${xcver}" > $WORKSPACE/copyBack/skipping_build
-    exit 0
-  elif [[ ${basequal} == e1[045] ]] && [[ ${OSnum} > 16 ]]
-  then
-    echo "${basequal} build not supported on `uname -s`${OSnum}"
-    echo "${basequal} build not supported on `uname -s`${OSnum}" > $WORKSPACE/copyBack/skipping_build
-  fi
-  if [[ ${d16_ok} == false ]] && [[ ${OSnum} > 15 ]]
-  then
-    echo "${basequal} build not supported on `uname -s`${OSnum}"
-    echo "${basequal} build not supported on `uname -s`${OSnum}" > $WORKSPACE/copyBack/skipping_build
-    exit 0
-  fi
-  # using this to disable unsupported El Capitan c2 builds
-  if [[ ${d14_ok} == false ]] && [[ ${OSnum} < 16 ]]
-  then
-    echo "${basequal} build not supported on `uname -s`${OSnum}"
-    echo "${basequal} build not supported on `uname -s`${OSnum}" > $WORKSPACE/copyBack/skipping_build
-    exit 0
-  fi
-fi
-
-dotver=`echo ${version} | sed -e 's/_/./g' | sed -e 's/^v//'`
-
-echo "building the larsoftobj distribution for ${version} ${dotver} ${qual_set} ${build_type}"
-
+# Find platform flavor.
 OS=`uname`
 if [ "${OS}" = "Linux" ]
 then
@@ -112,19 +82,66 @@ then
   if [ "${id}" = "Ubuntu" ]
   then
     flvr=u`lsb_release -r | sed -e 's/[[:space:]]//g' | cut -f2 -d":" | cut -f1 -d"."`
-    if [ "${flvr}" = "u14" ]; then
-      export UPS_OVERRIDE="-H Linux64bit+3.19-2.19"
-    fi
   else
     flvr=slf`lsb_release -r | sed -e 's/[[:space:]]//g' | cut -f2 -d":" | cut -f1 -d"."`
   fi
 elif [ "${OS}" = "Darwin" ]
 then
   flvr=d`uname -r | cut -f1 -d"."`
+  # set locale
+  echo
+  locale
+  echo
+  export LANG=C
+  export LC_ALL=$LANG
+  locale
+  echo
 else 
   echo "ERROR: unrecognized operating system ${OS}"
   exit 1
 fi
+
+# Check supported builds.
+if [[ `uname -s` == Darwin ]]; then
+  OSnum=`uname -r | cut -f1 -d"."`
+  xver=`xcodebuild -version | grep Xcode | cut -f2 -d" " | cut -f1 -d"."`
+  xcver=`xcodebuild -version | grep Xcode`
+  if { [[ ${basequal} =~ ^e(9$|[1-9][0-9]) ]] && \
+    [[ ${xver} < 7 ]] && \
+    [[ ${OSnum} > 13 ]]; }; then
+    # XCode too old on this platform.
+    echo "${basequal} build not supported on `uname -s`${OSnum} with ${xcver}"
+    echo "${basequal} build not supported on `uname -s`${OSnum} with ${xcver}" > \
+      "${working_dir}/copyBack/skipping_build"
+    exit 0
+  elif { [[ "$basequal" == e* ]] || \
+    { [[ "$basequal" == c* ]] && (( $OSnum < 15 )); }; }; then
+    if want_unsupported; then
+      echo "Building unsupported ${basequal} on `uname -s`${OSnum} due to \$CET_BUILD_UNSUPPORTED=${CET_BUILD_UNSUPPORTED}"
+    else
+      # Don't normally support GCC builds on MacOS.
+      msg="${basequal} build not supported on `uname -s`${OSnum} -- export CET_BUILD_UNSUPPORTED=1 to override."
+      echo "$msg"
+      echo "$msg" > "${working_dir}/copyBack/skipping_build"
+      exit 0
+    fi
+  fi
+  if have_label py3; then
+    msg="We are not building for Python3 on Darwin."
+    echo "${msg}"
+    echo "${msg}" > "${working_dir}/copyBack/skipping_build"
+    exit 0
+  fi
+elif [[ "${flvr}" == slf6 ]] && have_label py3; then
+    msg="Python3 builds not supported on SLF6."
+    echo "${msg}"
+    echo "${msg}" > "${working_dir}/copyBack/skipping_build"
+    exit 0
+fi
+
+dotver=`echo ${version} | sed -e 's/_/./g' | sed -e 's/^v//'`
+
+echo "building the larsoftobj distribution for ${version} ${dotver} ${qual_set} ${build_type}"
 echo "build flavor is ${flvr}"
 echo ""
 
@@ -142,23 +159,17 @@ mkdir -p ${srcdir} || exit 1
 mkdir -p ${blddir} || exit 1
 
 cd ${blddir} || exit 1
-curl --fail --silent --location --insecure -O http://scisoft.fnal.gov/scisoft/bundles/tools/pullProducts || exit 1
-chmod +x pullProducts
-# source code tarballs MUST be pulled first
-./pullProducts ${blddir} source larsoftobj-${version} || \
-      { cat 1>&2 <<EOF
-ERROR: pull of larsoftobj-${version} failed
-EOF
-        exit 1
-      }
-mv ${blddir}/*source* ${srcdir}/
+curl --fail --silent --location --insecure -O http://scisoft.fnal.gov/scisoft/bundles/tools/buildFW || exit 1
+chmod +x buildFW
 
 cd ${blddir} || exit 1
 echo
 echo "begin build"
 echo
-./buildFW -t -b ${basequal} ${blddir} ${build_type} larsoftobj-${version} || \
- { mv ${blddir}/*.log  $WORKSPACE/copyBack/
+./buildFW -t -b ${basequal} \
+  ${lopt} $(IFS=:; printf '%s' "${labels[*]}") \
+  ${blddir} ${build_type} larsoftobj-${version} || \
+ { mv ${blddir}/*.log  "${working_dir}/copyBack/"
    exit 1 
  }
 
